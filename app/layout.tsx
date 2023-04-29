5e40a6cbe7fc42c790cd5e9f1df422509e890f77
xcpp <xp2007@msn.com>
2023-04-29 11:38:02 +0800

    modify

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "OpenChat",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "OpenChat",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
